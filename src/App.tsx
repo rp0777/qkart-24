03fe78c47c842564247e9d28edd305e888786eaa
unknown <rp0777>
2024-05-20 08:34:04 +0530

    first commit: Initial setup with Vite, typescript & tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" h-screen w-screen flex justify-center items-center">
+      <p className=" text-3xl text-green-800 font-bold">Qkart</p>
+    </div>
+  );
+}
+
+export default App;
